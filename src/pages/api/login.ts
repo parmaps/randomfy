7b67f48716101986a3aa025356c078b78a6b36c9
donmerengue <l.parma@hotmail.com>
2023-06-08 12:09:01 -0300

    login setup with next api routes, .env variables and typescript

---
+++ src/pages/api/login.ts
@@ -0,0 +1,27 @@
+// Initiate the authorization request
+import type { NextApiRequest, NextApiResponse } from "next";
+const querystring = require("querystring");
+
+type Data = {
+  name: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // var state = generateRandomString(16);
+  const scope: string = "user-read-private user-read-email";
+  const redirect_uri: string = process.env.NEXT_PUBLIC_REDIRECT_URI as string;
+
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        // state: state
+      })
+  );
+}

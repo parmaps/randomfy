6e8881bf86673757b4452fc2a87cb21c86875f25
donmerengue <l.parma@hotmail.com>
2023-06-09 10:51:10 -0300

    auth code flow: request access token and provide callback

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -12,13 +12,14 @@ export default function handler(
 ) {
   // var state = generateRandomString(16);
   const scope: string = "user-read-private user-read-email";
+  const client_id = process.env.NEXT_PUBLIC_CLIENT_ID;
   const redirect_uri: string = process.env.NEXT_PUBLIC_REDIRECT_URI as string;
 
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
       querystring.stringify({
         response_type: "code",
-        client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+        client_id: client_id,
         scope: scope,
         redirect_uri: redirect_uri,
         // state: state

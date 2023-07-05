3b8981631fd36069b76c0f16cc963c31f9053490
donmerengue <l.parma@hotmail.com>
2023-07-05 16:09:20 -0300

    Sequelize db connection

---
+++ src/database/index.ts
@@ -0,0 +1,15 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("randomfy", "mapi", "Merengue1", {
+  host: "localhost",
+  dialect: "postgres",
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();

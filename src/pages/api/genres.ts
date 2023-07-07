0993d1885a125919907527477a4827023c20c096
donmerengue <l.parma@hotmail.com>
2023-07-07 15:16:43 -0300

    Get Genres logic

--- src/pages/api/genres.ts
+++ src/pages/api/genres.ts
@@ -1,10 +1,11 @@
-import createBulkGenres from "@/controllers/genreController";
+import { createBulkGenres, getGenres } from "@/controllers/genreController";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") await createBulkGenres(req, res);
+  if (req.method === "GET") await getGenres(req, res);
+  else if (req.method === "POST") await createBulkGenres(req, res);
   else res.status(405).json({ error: "Method not allowed" });
 }

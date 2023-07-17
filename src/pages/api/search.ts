76e6f982fe0b777190fda958641e4f4f2c28ff57
donmerengue <l.parma@hotmail.com>
2023-07-17 13:38:37 -0300

    Refactor API search with switch/case & error handling

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,10 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { createSearch } from "@/controllers/searchController";
 
-export default async function hanlder(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") await createSearch(req, res);
-  else res.status(405).json({ error: "Method not allowed" });
+  const { method } = req;
+
+  switch (method) {
+    case "POST":
+      try {
+        await createSearch(req, res);
+        res.status(201).json({ message: "Search created successfully" });
+      } catch (error) {
+        console.error("Error creating search:", error);
+        res.status(500).json({ error: "Internal server error" });
+      }
+      break;
+    default:
+      res.status(405).json({ error: "Method not allowed" });
+      break;
+  }
 }

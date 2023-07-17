4837791920b8b611ab6d05029fddb151539e8059
donmerengue <l.parma@hotmail.com>
2023-07-17 13:59:17 -0300

    Refactor API search with switch/case & error handling

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -17,6 +17,17 @@ export default async function handler(
         res.status(500).json({ error: "Internal server error" });
       }
       break;
+
+    //   case "GET":
+    //     try {
+    //       // TODO 17/7 Handle GET request logic here (await getSearch)...
+    //       res.status(200).json({ message: "GET request handled" });
+    //     } catch (error) {
+    //       console.error("Error handling GET request:", error);
+    //       res.status(500).json({ error: "Internal server error" });
+    //     }
+    //     break;
+
     default:
       res.status(405).json({ error: "Method not allowed" });
       break;

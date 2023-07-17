81e1896d860e00d18c0b65ac3267160cb9649fca
donmerengue <l.parma@hotmail.com>
2023-07-17 14:01:49 -0300

    Refactor API users with switch/case & error handling

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -5,7 +5,31 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") await createUser(req, res);
-  // if (req.method === "POST") await getUser
-  else res.status(405).json({ error: "Method not allowed" });
+  const { method } = req;
+
+  switch (method) {
+    case "POST":
+      try {
+        await createUser(req, res);
+        res.status(201).json({ message: "User created successfully" });
+      } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).json({ error: "Internal server error" });
+      }
+      break;
+
+    // case "GET":
+    //   try {
+    //     // TODO 17/7 Handle GET request logic here (await getUser)...
+    //     res.status(200).json({ message: "GET request handled" });
+    //   } catch (error) {
+    //     console.error("Error handling GET request:", error);
+    //     res.status(500).json({ error: "Internal server error" });
+    //   }
+    //   break;
+
+    default:
+      res.status(405).json({ error: "Method not allowed" });
+      break;
+  }
 }

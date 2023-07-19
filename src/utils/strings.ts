a26d53f57f818f0cd4a36cb8f48c75703f5a2394
donmerengue <l.parma@hotmail.com>
2023-07-19 18:51:52 -0300

    Add function to format genres

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -3,12 +3,6 @@ export const capitalizeWord = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
-export const map = async (genresList: string[]) => {
-  return genresList.map((genre) => ({
-    label: capitalizeWord(genre),
-    value: genre,
-  }));
-};
 
 export const mapGenresToOptionsValues = async (
   genresObjectList: GenreObject[]
